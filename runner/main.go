ec24fbba673477e42c617afa3716883d0e2feb70
damouse <damouse007@gmail.com>
2016-06-16 22:00:49 -0500

    google searching

--- runner/main.go
+++ runner/main.go
@@ -5,44 +5,77 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/hokaccha/go-prettyjson"
+	"net/url"
 )
 
-// Link to CSE docs: https://developers.google.com/custom-search/json-api/v1/reference/cse/list#request
+const baseUrlString string = "https://www.googleapis.com/customsearch/v1"
 
-const apiKey string = "AIzaSyBSX7Xr-juL_3oTB3ZQaj5Wmcv1__kfR0w"
-
-func main() {
-	// illmenu.TestSearch()
-	// Other important keys: safe and searchType
-	searchUrl := "https://www.googleapis.com/customsearch/v1?q=dog&cx=013062920850834713833:hsd8bzw_nki&key=AIzaSyBSX7Xr-juL_3oTB3ZQaj5Wmcv1__kfR0w"
-
-	res, err := http.Get(searchUrl)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	s, err := getStations([]byte(body))
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p, _ := prettyjson.Marshal(s)
-	fmt.Println(string(p))
+var params map[string]string = map[string]string{
+	"key":        "AIzaSyBSX7Xr-juL_3oTB3ZQaj5Wmcv1__kfR0w",
+	"cx":         "013062920850834713833:hsd8bzw_nki",
+	"safe":       "high",
+	"searchType": "image",
 }
 
-func getStations(body []byte) (map[string]interface{}, error) {
-	s := map[string]interface{}{}
-
-	err := json.Unmarshal(body, &s)
-	if err != nil {
-		fmt.Println("whoops:", err)
+func main() {
+	links, e := ImageSearch("dogs")
+	if e != nil {
+		panic(e.Error())
 	}
-	return s, err
+
+	fmt.Println(links)
+}
+
+// Searches google images with the given query, and returns a list of image urls
+// Link to CSE docs: https://developers.google.com/custom-search/json-api/v1/reference/cse/list#request
+func ImageSearch(query string) ([]string, error) {
+	base, e := url.Parse(baseUrlString)
+	if e != nil {
+		return nil, e
+	}
+
+	// Build the query
+	q := base.Query()
+	q.Add("q", query)
+
+	for k, v := range params {
+		q.Add(k, v)
+	}
+
+	base.RawQuery = q.Encode()
+
+	// Make the request
+	fullJson := map[string]interface{}{}
+
+	if res, e := http.Get(base.String()); e != nil {
+		return nil, e
+	} else if body, e := ioutil.ReadAll(res.Body); e != nil {
+		return nil, e
+	} else if e := json.Unmarshal(body, &fullJson); e != nil {
+		return nil, e
+	} else if res.Status != "200 OK" {
+		return nil, fmt.Errorf("Google search failed: %v", fullJson)
+	}
+
+	// Extract the values from the json
+	links := make([]string, 0)
+
+	if results, ok := fullJson["items"].([]interface{}); !ok {
+		fmt.Println("Cast failed! heres the json: ", fullJson)
+		return nil, e
+	} else {
+		for _, result := range results {
+			if j, ok := result.(map[string]interface{}); !ok {
+				return nil, fmt.Errorf("Cast failed! Bad result type")
+			} else if link, ok := j["link"]; !ok {
+				return nil, e
+			} else if s, ok := link.(string); !ok {
+				return nil, e
+			} else {
+				links = append(links, s)
+			}
+		}
+	}
+
+	return links, nil
 }
